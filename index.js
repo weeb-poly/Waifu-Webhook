bc88209a6022565d325ce5658ea87f579fb33d49
Prasantacharya <acharp@rpi.edu>
2021-04-16 18:42:49 -0400

    gonna rename the repo

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const webhook = require("webhook-discord")
 const puppeteer = require("puppeteer")
-const Hook = new webhook.Webhook("")
 
-// searches using puppeteer and wikicommons
-async function searchInWiki(item){
-
-}
-
-// generic search function to anywhere
-async function searchAnything(item, url){
-
-}
 
 
