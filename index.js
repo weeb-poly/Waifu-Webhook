a380f31e379d553912fef413a5019a11a3a26196
Prasantacharya <acharp@rpi.edu>
2021-04-17 16:26:06 -0400

    downloading should work, now trying to implement the webhook

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const webhook = require("webhook-discord");
+// const webhook = require("webhook-discord");
 const https = require('https');
 fs = require('fs');
 
 
-function download(url, imagename){
-	https.get(url, (resp) => {
+function download(url, dir, imagename){
+    if(!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+    https.get(url, (resp) => {
 		let data = '';
 		resp.setEncoding('binary');
 		resp.on('data', (chunk) => {
 			data += chunk;
 		});
 		resp.on('end', () => {
-			fs.writeFile(imagename, data, 'binary', (err) => {
+			fs.writeFile(dir + '/' + imagename, data, 'binary', (err) => {
             if (err) throw err;
             console.log('File saved.');
       });
@@ -20,31 +22,50 @@ function download(url, imagename){
 	});
 }
 
-async function start(schedule, downloads){
-	Object.keys(schedule).forEach((key) => {
-		await https.get("https://waifu.pics/api/", () => {
-			let data = '';
-			resp.on('data', (chunk) = > {
-				data += chunk;
-			});
+async function start(schedule){
 
-			resp.on('end', () => {
-				let imgData = JSON.parse(data);
-				let url = imgData["url"];
+    let endpoint = "https://waifu.pics/api/" + schedule[key][sfw] + '/';
+  		await https.get(endpoint, (resp) => {
+  			let data = '';
+  			resp.on('data', (chunk) => {
+  			    data += chunk;
+  			});
 
-				download(url, name));
-			});
+  			resp.on('end', () => {
+  			    let imgData = JSON.parse(data);
+  			    let url = imgData["url"];
+            // regex for getting the file type extension
+            let name = key + url.match(/\.[0-9a-z]+$/i);
+  			    download(url, "./" + key, name);
+  			});
 
-			resp.on('error', (err) => {
-				console.log("Error: " + err.message);
-			});
+  			resp.on('error', (err) => {
+  			    console.log("Error: " + err.message);
+  			});
 
-		});
-	});
+  		});
 
 };
 
-// main function
+async function driver(schedule){
+
+  let tags = schedule[key][tags].match(/[^,]+/g); // array of tags
+
+  /*
+  for await(){
+
+  }
+  */
+}
+
+// this is the main function
+(() => {
+  let rawData = fs.readFileSync("./config/channels.json");
+  let schedule = JSON.parse(rawData);
+  driver(schedule);
+})();
+
+/* main function
 (() => {
     let schedule = {};
     // parse the sites and channels
@@ -53,3 +74,4 @@ async function start(schedule, downloads){
     let downloads = start(schedule);
 
 })();
+*/
